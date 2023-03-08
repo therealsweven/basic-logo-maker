9e2efc87ded3ddb14fc9d1ef0cdc20f8dc3faee1
therealsweven <robssimpson11@gmail.com>
2023-03-08 03:06:05 -0700

    cleaned up code and added a walkthrough video to repo

--- index.js
+++ index.js
@@ -1,36 +1,7 @@
-// Constructor Arithmetic is imported.
-const Triangle = require("./lib/triangle.js");
-const Square = require("./lib/square.js");
-const Circle = require("./lib/circle.js");
 const CLI = require("./lib/CLI.js");
 const fs = require("fs");
-const inquirer = require("inquirer");
 
-const questions = [
-  {
-    type: "input",
-    message: "What text would you like to be on the logo? (3 character max)",
-    name: "text",
-  },
-  {
-    type: "input",
-    message: "What color would you like the logo to be?",
-    name: "bgColor",
-  },
-  {
-    type: "input",
-    message: "What color would you like the text to be?",
-    name: "textColor",
-  },
-  {
-    type: "list",
-    message: "What shape would you like the logo to be?",
-    choices: ["square", "triangle", "circle"],
-    name: "shape",
-  },
-];
-
-const cli = new CLI(questions);
+const cli = new CLI();
 // // console.log(cli);
 cli.run().then((logoString) => {
   fs.writeFile("./dist/logo.svg", logoString, (error) => {
